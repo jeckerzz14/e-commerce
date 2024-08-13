ef3b5423acbddbcec20b6d5d7315e66335a3c59e
Jerick Torres <torres.jerick014@gmail.com>
2024-08-13 17:23:05 +0800

    product list component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,75 @@
 <template>
     <v-app>
         <v-main>
-            <v-container>
-                <span class="font-light-italic">HOMEPAGE</span>
+            <!-- TODO make layouts to header and footer like in nuxt -->
+            <!-- HEADER -->
+            <v-app-bar :elevation="2">
+                <v-app-bar-title>Ecommerce</v-app-bar-title>
+
+                <div class="card-wrapper">Cart-icon</div>
+                <div class="login-wrapper">Login</div>
+            </v-app-bar>
+            
+            <!-- Category list -->
+            
+            <!-- Product list -->
+            <v-container class="v-col-9">
+                <h2>Products</h2>
+                <v-row v-if="!products.isLoading">
+                    <v-col v-for="product in products.data" :key="product.id" cols="12" md="2">
+                        <product-list :product="product"/>
+                    </v-col>
+                </v-row>
+                <v-skeleton-loader type="card" v-else></v-skeleton-loader>
             </v-container>
+
+
+            <!-- FOOTER -->
+            <v-bottom-navigation></v-bottom-navigation>
         </v-main>
     </v-app>
 </template>
 
 <script>
+// Components
+import ProductList from "@/views/ProductList.vue";
+
+// API
 import productsApi from "@/api/productsApi.js";
+
 
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    ProductList
+  },
   data() {
     return {
-      products: [],
+      products: {
+        data: [],
+        isLoading: false,
+      },
+      categories: {
+        data: [],
+        isLoading: false,
+      },
     };
   },
-  mounted() {
-    this.getAllProducts();
+  async mounted() {
+    await this.getAllProducts();
   },
   methods: {
+    // TODO include in mixins
     async getAllProducts() {
       try {
+        this.products.isLoading = true;
         const response = await productsApi.getAllProducts();
-        this.products = response.data;
+        this.products.data = response.data;
+        this.products.isLoading = false;
+
       } catch (error) {
         console.error("HomeView - getAllProducts - Error fetching data:", error);
+        this.products.isLoading = false;
       }
     },
   },
---
+++ src/views/ProductList.vue
@@ -0,0 +1,51 @@
+<template>
+<section @click="viewProduct(product)">
+    <v-card
+        class="mx-auto cursor-pointer"
+        max-width="200"
+    >
+        <v-img
+            :src="product?.images[0]"
+            :alt="`product image ${product?.category.name}`"
+            class="zoom-on-hover"
+            height="200"
+            cover
+        >
+        </v-img>
+
+        <v-card-title class="font-bold"> {{ product?.title }} </v-card-title>
+
+        <v-card-subtitle> {{ product?.category.name }} </v-card-subtitle>
+        <v-card-subtitle class="price"> ₱{{ product?.price }} </v-card-subtitle>
+    </v-card>
+</section>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  props: {
+    product: {
+        type: Object,
+        required: true,
+    },
+  },
+  methods: {
+    viewProduct(data) {
+        console.log('view product -', data.id, data.price);
+
+        // router.push /product/:id
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+    @import "@/assets/styles/settings/_colors.scss";
+
+    .price {
+        font-size: 1.125rem;
+        color: $warning;
+        opacity: 100;
+    }
+</style>
